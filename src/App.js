e82dace1ea892c5d8b9a045cfe062c3a702baef5
vivektangudu123 <vivektangudu2383@gmail.com>
2023-11-13 22:56:50 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Button, Flex } from 'antd';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>
+           Library
+      </h1>
+
+        <Flex gap="small" wrap="wrap">
+            <Button type="primary">Primary Button</Button>
+            <Button>Default Button</Button>
+            <Button type="dashed">Dashed Button</Button>
+            <Button type="text">Text Button</Button>
+            <Button type="link">Link Button</Button>
+        </Flex>
     </div>
   );
 }
