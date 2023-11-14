cc7e1c2d2ce9c3d090c40fef3eb7743ee2bba8b8
vivektangudu123 <vivektangudu2383@gmail.com>
2023-11-14 23:52:20 +0530

    front-end done

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import {Form} from 'antd';
+import Button from "../../components/Button";
+import {Link} from "react-router-dom";
 function Login(){
+    const onFinish=(values)=>{console.log("Success",values)};
     return(
-        <div>Login</div>
+        <div className="h_screen bg_primary flex items-center justify-center">
+            <div className="authentication_form bg_white p_3">
+                <h1 className="text_secondary text_xxl">
+                    Library-Login
+                </h1>
+                <hr/>
+                <Form layout='vertical' onFinish={onFinish} >
+                    <Form.Item label='Email' name='email'>
+                        <input type='email' placeholder='Email'></input>
+                    </Form.Item>
+
+                    <Form.Item label='Password' name='password'>
+                        <input type='password' placeholder='Password'></input>
+                    </Form.Item>
+                    <Button title="Register" type="submit" color="secondary" variant="outlined" />
+                    <div className="text_center mt_2">
+                        <Link to="/Register"  className="text_primary">Create a new account?</Link>
+                    </div>
+                </Form>
+            </div>
+        </div>
     )
 }
 export default Login
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,8 +1,35 @@
 import React from 'react'
-import Login from "../Login";
+import {Form} from 'antd';
+import Button from "../../components/Button";
+import {Link} from "react-router-dom";
 function Register(){
+    const onFinish=(values)=>{console.log("Success",values)};
     return(
-        <div>Register</div>
+        <div className="h_screen bg_primary flex items-center justify-center">
+            <div className="authentication_form bg_white p_3">
+                <h1 className="text_secondary text_xxl">
+                    Library-Register
+                </h1>
+                <hr/>
+                <Form layout='vertical' onFinish={onFinish} >
+                    <Form.Item label='Name' name='name'>
+                        <input type='text' placeholder='Name'></input>
+                    </Form.Item>
+
+                    <Form.Item label='Email' name='email'>
+                        <input type='email' placeholder='Email'></input>
+                    </Form.Item>
+
+                    <Form.Item label='Password' name='password'>
+                        <input type='password' placeholder='Password'></input>
+                    </Form.Item>
+                    <Button title="Register" type="submit" color="secondary" variant="outlined" />
+                    <div className="text_center mt_2">
+                        <Link to="/login"  className="text_primary">Already have an account?</Link>
+                    </div>
+                </Form>
+            </div>
+        </div>
     )
 }
 export default Register
