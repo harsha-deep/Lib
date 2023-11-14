99d0064ddc44b3228271b5167b40a40bef06d615
vivektangudu123 <vivektangudu2383@gmail.com>
2023-11-14 15:08:53 +0530

    Login folders

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import { Button, Flex } from 'antd';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import './stylesheets/alignment.css';
+import './stylesheets/theme.css';
+import './stylesheets/sizes.css';
+import './stylesheets/custome-components.css';
+import './stylesheets/form-elements.css';
+
 function App() {
   return (
-    <div >
-      <h1>
-           Library
-      </h1>
-
-        <Flex gap="small" wrap="wrap">
-            <Button type="primary">Primary Button</Button>
-            <Button>Default Button</Button>
-            <Button type="dashed">Dashed Button</Button>
-            <Button type="text">Text Button</Button>
-            <Button type="link">Link Button</Button>
-        </Flex>
+    <div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element= {<Home/>}/>
+                <Route path="/login" element= {<Login/>}/>
+                <Route path="/register" element= {<Register/>}/>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Register/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Login from "../Login";
+function Register(){
+    return(
+        <div>Register</div>
+    )
+}
+export default Register
